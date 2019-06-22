7ea71f7d8c3df33f9896fd7058e6073e72f7ae4b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 21:08:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integeer :num_of_employees
+    end
+  end
+end
