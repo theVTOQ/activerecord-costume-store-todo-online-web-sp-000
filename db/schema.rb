413aebcebd0586128afa59f2f30e304dc365542c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 21:17:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.float "price"
     t.string "image_url"
+    t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
