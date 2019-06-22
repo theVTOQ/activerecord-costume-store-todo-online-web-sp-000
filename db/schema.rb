57eebdc0eedbc567657ba34da871b3f45efb3016
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 21:17:20 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.float "price"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
