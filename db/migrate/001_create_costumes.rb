3e9cd4478507a1d1f5db16672ddfb5438a3467d9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 20:47:01 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name,
-      t.float :price,
+      t.decimal :price,
       t.string :image_url
     end
   end
